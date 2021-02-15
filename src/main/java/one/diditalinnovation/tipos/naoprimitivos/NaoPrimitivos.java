0264bf072db15cb348ddb3460df42a062806ad02
Anderson Colin <andercol@gmail.com>
2021-02-15 19:34:30 -0300

    Adicionado tipos não primitivos e unboxing

---
+++ src/main/java/one/diditalinnovation/tipos/naoprimitivos/NaoPrimitivos.java
@@ -0,0 +1,19 @@
+package one.diditalinnovation.tipos.naoprimitivos;
+
+public class NaoPrimitivos {
+
+    public static void main(String[] args) {
+
+        String texto = "Meu texto para apresentação"; //Sequência de caracteres
+
+        Void v; //tipo válido.
+
+        Object o = new Object();
+
+        Number numero = Integer.valueOf(100);
+
+        numero.toString();
+        //utilizando os tipos não primitivos consigo utilizar todos os métodos.
+
+    }
+}
